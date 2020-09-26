31cda96b4739f0feb8fb5984e2f1d3f42792ec4b
notomo <notomo.motono@gmail.com>
2020-09-27 08:47:52 +0900

    Refactor test helper

--- test/lua/hita/source/search_spec.lua
+++ test/lua/hita/source/search_spec.lua
@@ -1,5 +1,4 @@
-local helper = require "test.helper"
-local assert = helper.assert
+local helper = require("hita/lib/testlib/helper")
 local command = helper.command
 
 describe("search source", function()
